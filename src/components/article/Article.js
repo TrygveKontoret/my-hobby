6054be9d98e27e51cea92b766c34533d5d4a7b27
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-09-01 14:35:38 +0200

    added gradient

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,10 +1,11 @@
 import {wrapper} from './article.module.scss';
 
 const Article = (props) => {
-    const {title, bodytext, img, alt} = props;
+    const {title, bodytext, img, alt, quote} = props;
     return (
         <div className={wrapper}>
             <h2>{title}</h2>
+            <p>{quote}</p>
             <div>
             <img src={img} alt={alt}/>
             <p>{bodytext}</p>
